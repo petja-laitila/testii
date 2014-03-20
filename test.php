16ef7acf2df6b30877e68d7a2ae1f0783b3300b8
petja.laitila@otavanopisto.fi <petja.laitila@otavanopisto.fi>
2014-03-20 09:29:49 +0200

    blaa blaa

--- test.php
+++ test.php
@@ -1,8 +1,21 @@
+<!DOCTYPE html PUBLIC "-//W3C//DTD HTML 4.01 Transitional//EN" "http://www.w3.org/TR/html4/loose.dtd">
+<html>
+<head>
+<meta http-equiv="Content-Type" content="text/html; charset=UTF-8">
+<title>Insert title here</title>
+</head>
+<body>
+<form action="test.php" method="post">
+Project name: <input type="text" name="project"><br>
+<input type="submit" value="Submit">
+</form>
 <?php
-  phpinfo();
-  $taulu = array('jukka' => 'poika', 'sipuli' => 'peruna');
-  foreach ($taulu as $key => $value){
-  	echo $key . '<br>';
-  	echo $value;
-  }
-  ?>
+$get_params = $_GET['saibaa'];
+$post_params = $_POST['project'];
+  $h = fopen('param_log', 'a');
+  fwrite($h, 'POST params: ' . $post_params . "\n");
+  fwrite($h, 'GET params: ' . $get_params . "\n");
+  fclose($h);
+?>
+</body>
+</html>
